26490e5341085326a4db8cd74843a743fa3cdcf4
Wajid Ali <wajid786ali@gmail.com>
2023-06-20 22:15:40 +0530

    weekly worksheet changes

--- src/app/create-worksheets/create-worksheets.component.ts
+++ src/app/create-worksheets/create-worksheets.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Worksheets } from '../entity/worksheets';
+import { ApiClientService } from '../service/api-client.service'; 
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-worksheets',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-worksheets.component.css']
 })
 export class CreateWorksheetsComponent {
+  
+  worksheet: Worksheets = new Worksheets();
+  message: string;
+  students: any;
+  filterWorksheet!: string;
+  constructor(private apiService: ApiClientService,
+    private router: Router
+  ) { }
 
+  ngOnInit() {
+    this.readAll();
+  }
+
+  readAll() {
+    let response1 = this.apiService.getAll();
+    response1.subscribe((data1) => {
+      this.students = data1;
+    });
+  }
+
+  wsSubmit(worksheets: Worksheets) {
+     
+  }
 }
